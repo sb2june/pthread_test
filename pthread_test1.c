4668fb5ffe96ddeeb2c1e68c889d510d2671d709
sungbum22.park <sungbum22.park@lge.com>
2015-04-23 11:54:10 +0900

    delete 1 line, rename the fun Thread->myThread

--- pthread_test1.c
+++ pthread_test1.c
@@ -3,7 +3,6 @@
  * Title: Creating/terminating threads(1)
  * Desc: 쓰레드 생성 제거 예제
  * Revision History
- * 
  ***************************************/
 #include <stdio.h>
 #include <pthread.h>
@@ -11,7 +10,7 @@
 int thread_args[4] = { 0, 1, 2 ,3};  /* 쓰레드가 사용할 인자 */
 //-------------------------------------------------------------
 /* 쓰레드로 수행할 함수 */
-void* Thread( void *arg )
+void* myThread( void *arg )
 {
     int i;
     for ( i=0; i<3; i++ )
@@ -28,7 +27,7 @@ int main( void )
         // TODO: 스레드 생성하기
         pthread_create( &threads[i],                /* 쓰레드ID */
                         NULL,                       /* 쓰레드 속성 */
-                        ( void* (*)(void*) )Thread, /* 쓰레드 시작 함수 */
+                        ( void* (*)(void*) )myThread, /* 쓰레드 시작 함수 */
                         &thread_args[i] );          /* 생성된 쓰레드에 전달 인자 */
     
     pthread_exit(0); /*메인 쓰레드 종료 */
